84db3d6ace7d65e6a3d699389de236fe52c75797
Hardik Shah <roophardik@gmail.com>
2020-05-03 14:52:59 +0530

    edit commit

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -43,7 +43,7 @@ const Login = ({login, isAuthenticated}) => {
                     onChange={e => onChange(e)}                        
                     />
                 </div>
-                <input type="submit" className="btn btn-primary" value="Register" />
+                <input type="submit" className="btn btn-primary" value="Login" />
             </form>
             <p className="my-1">
                 Don't have an account? <Link to='/register'>Sign Up</Link>
